baaf9faacd6bf5d8a918b5be309616959dc51fc8
prazhusleecooper <prashanth5454@gmail.com>
2022-11-04 23:03:23 +0530

    stepBar spacing fix

--- src/Components/StepBar/StepBar.js
+++ src/Components/StepBar/StepBar.js
@@ -6,7 +6,7 @@ import { FaCheck } from 'react-icons/fa';
 const StepBar = () => {
 
     return(
-        <div className='w-full d-flex flex-row items-start justify-between px-20 py-3 mb-5'>
+        <div className='w-full d-flex flex-row items-start justify-between pl-20 pr-24 py-3 mb-5'>
             <div className='d-flex flex-column items-center justify-center gap-2'>
                 <div className='bg-secondaryBlue-default text-white text-[22px] w-[45px] h-[45px] rounded-full d-flex flex-row items-center justify-center'>
                     <FaCheck />
